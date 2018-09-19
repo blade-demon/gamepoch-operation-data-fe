fe73054c73a261276f177a9c7173bf3f1750fd00
xuziwei <xuziwei89@gmail.com>
2018-09-19 16:39:26 +0800

    First commit

---
+++ src/components/SimpleTabs.js
@@ -0,0 +1,77 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper
+  }
+});
+
+class SimpleTabs extends React.Component {
+  state = {
+    value: 0,
+    imgSrc: [
+      "https://news-summary-ziweigamepoch.c9users.io/img/head.png",
+      "https://news-summary-ziweigamepoch.c9users.io/img/fans.png",
+      "https://news-summary-ziweigamepoch.c9users.io/img/blog.png",
+      "https://news-summary-ziweigamepoch.c9users.io/img/interact.png",
+      "https://news-summary-ziweigamepoch.c9users.io/img/article.png"
+    ]
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Tabs value={value} onChange={this.handleChange}>
+            {this.props.tabItems.map((tab, index) => (
+              <Tab key={index} label={tab} />
+            ))}
+          </Tabs>
+        </AppBar>
+        {this.props.tabItems.map(
+          (item, index) =>
+            this.state.value === index && (
+              <TabContainer>
+                <img
+                  src={this.state.imgSrc[index]}
+                  alt="微博数据"
+                  style={{ width: "100%" }}
+                />
+              </TabContainer>
+            )
+        )}
+      </div>
+    );
+  }
+}
+
+SimpleTabs.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(SimpleTabs);
--- src/pages/Weibo.js
+++ src/pages/Weibo.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
+import CustomAppBar from "../components/CustomAppBar";
+import SimpleTabs from "../components/SimpleTabs";
 
 export default class Weibo extends Component {
+  state = {
+    tabItems: ["数据概览", "粉丝分析", "博文分析", "互动分析", "文章分析"]
+  };
+
   render() {
-    return <div>暂未开放敬请期待！</div>;
+    return (
+      <div>
+        <CustomAppBar title="微博数据" />
+        <SimpleTabs tabItems={this.state.tabItems} />
+      </div>
+    );
   }
 }
